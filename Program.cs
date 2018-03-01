adad848151173f44191bc6bd131157ffab107b1f
Victor Gavrish <victor_gavrish@epam.com>
2018-03-01 14:51:46 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace AspnetcoreHelloworld
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebHost.StartWith(app => app.Run(async (context) => await context.Response.WriteAsync("Hello World!"))).WaitForShutdown();
+        }
+    }
+}
